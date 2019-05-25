b1c45ae6a8be0a448b2aa8c07ead23aea323b6ea
svaus <sanket_verma@outlook.com>
2019-05-25 11:59:51 +1000

    feat(initial): Intial commit for SPA react application

---
+++ single-spa.config.js
@@ -0,0 +1,33 @@
+import { registerApplication, start } from 'single-spa'
+
+function pathPrefix(prefix) {
+    return function(location) {
+        return location.pathname.startsWith(prefix);
+    }
+}
+
+
+registerApplication(
+    'navBar', 
+    () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
+    () => true
+  );
+
+registerApplication(
+  // Name of our single-spa application
+  'home',
+  // loadingFunction
+  () => import('./src/home/home.app.js'),
+  // activityFunction
+  (location) => location.pathname === "" || 
+    location.pathname === "/" || 
+    location.pathname.startsWith('/home')
+);
+
+registerApplication(
+    'angularJS', 
+    () => import ('./src/angularJS/angularJS.app.js'), 
+    pathPrefix('/angularJS')
+  );
+
+start();
